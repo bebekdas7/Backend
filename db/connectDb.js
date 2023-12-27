ce0d9c713cabab26515c497572dae141fe463ecc
Bebek Das <bebekdas7@gmail.com>
2023-12-27 15:31:46 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -9,8 +9,12 @@ mongoose.set('strictQuery',true)
 // helper function for connection
 const connectDb = async (databaseUrl) => {
     try {
-        await mongoose.connect(databaseUrl, options);
-        console.log("Connected Successfully")
+    await mongoose.connect(databaseUrl, {
+            ...options,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });        
+    console.log("Connected Successfully")
     }
     catch (error) {
         console.log("Error catched in connectDb.js",error)
